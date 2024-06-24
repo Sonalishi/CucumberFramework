c1b0c4dfd7932e2da49ee861ce86010bd948d562
shind <shind@sonali>
2024-06-24 16:52:01 +0200

    Successful registration

--- src/test/Resources/Features/Registration.feature
+++ src/test/Resources/Features/Registration.feature
@@ -4,6 +4,13 @@ Feature: Registration Functionality for nopCommerce site
   Also want to create my login Id and password 
   So that I can shop the goods.
 
-  Scenario: Successful login to registration page
-    Given I on registration page
-    When I retest
+  Background: Successful login to registration page
+   Given I on registration page
+   
+  Scenario Outline: Successful registration with valid credentials
+    Given I have entered valid <Gender> , <FirstName> , <LastNme> , <Email> , <Password> , <ConfirmPassword>
+    When I click on Register button
+    Then I should be logged in Successfully
+    Examples: 
+      | Gender | FirstName | LastNme | Email               | Password | ConfirmPassword|
+      |Male    | Steve     | Jobs    | stevejobs@gmail.com | steveJ123| steveJ123      |
--- src/test/java/StepDefinations/RegistrationStepdefination.java
+++ src/test/java/StepDefinations/RegistrationStepdefination.java
@@ -3,8 +3,10 @@ package StepDefinations;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class RegistrationStepdefination {
@@ -18,14 +20,33 @@ public class RegistrationStepdefination {
 		driver=new ChromeDriver();
 	}
 
+	@After
+	public void teardown()
+	{
+		if(driver!=null)
+		{
+			driver.quit();
+		}
+	}
+
 	@Given("I on registration page")
 	public void i_on_registration_page() {
 	   driver.get("https://demo.nopcommerce.com/register?returnUrl=%2F");
-	   System.out.print("Hello");
+
 	}
 	
-	@When("I retest")
-	public void i_retest() {
-		 System.out.print("Hello Again");
+	@Given("^I have entered valid (.*) , (.*) , (.*) , (.*) , (.*) , (.*)$")
+	public void i_have_entered_valid_credentials(String gender,String firstname,String lastname,String email,String password, String confirmpassword) {
+	    System.out.println(gender + firstname);
+	}
+
+	@When("I click on Register button")
+	public void i_click_on_register_button() {
+	    System.out.println("Hello1");
+	}
+
+	@Then("I should be logged in Successfully")
+	public void i_should_be_logged_in_successfully() {
+	   System.out.println("Hello2");
 	}
 }
